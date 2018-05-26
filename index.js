66c767acaf8d7f6d3299213a892c6f66dff3bfb1
Sergei Selihov <red331@mail.ru>
2018-05-26 17:00:16 +0300

    Export fixes.

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ const i18nHelpers = require('./src/helpers/general/i18n_helpers');
 const utilityHelpers = require('./src/helpers/general/utility_helpers');
 
 // exports
-module.exports.helpers = {
-    i18nHelpers,
-    utilityHelpers,
-};
+module.exports.formatMessages = i18nHelpers.formatMessages;
+
+module.exports.isCanBeEmpty = utilityHelpers.isCanBeEmpty;
+module.exports.isNullOrUndefined = utilityHelpers.isNullOrUndefined;
+module.exports.isNotNullOrUndefined = utilityHelpers.isNotNullOrUndefined;
+module.exports.isNotNil = utilityHelpers.isNotNil;
+module.exports.isNotEmpty = utilityHelpers.isNotEmpty;
