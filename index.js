2d34ddcecaf7e172245890141a12f0005d36c7de
Sergei Selihov <red331@mail.ru>
2018-05-28 17:11:28 +0300

    Major fixes.

--- index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-// external imports
-
-// local imports
-
-// exports
-export * from './src/helpers/general/i18n_helpers';
-export * from './src/helpers/general/utility_helpers';
---
+++ src/helpers/general/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+// @flow
+
+// exports
+export * from './i18n_helpers';
+export * from './utility_helpers';
