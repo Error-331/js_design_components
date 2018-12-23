b78ca6bb692632c9be8bf53798fa98eb37671687
Sergei Selihov <red331@mail.ru>
2018-12-23 16:14:50 +0200

    Add types.

---
+++ src/types/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// @flow
+
+// external imports
+
+// internal imports
+
+// types export
+
+export type * from './general';
+export type * from './promise';
+export type * from './request_response';
+export type * from './web_futuristics';
---
+++ src/types/request_response.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// @flow
+
+// external imports
+
+// internal imports
+
+// types definition
+export type RequestHeaderType = {[string]: string};
+export type RequestHeadersType = Array<RequestHeaderType>;
